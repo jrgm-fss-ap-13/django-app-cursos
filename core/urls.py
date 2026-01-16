d5660fb1afe4782874d19199103121e00db81b74
jrgm-fss-ap-13 <jrgm.fss.ap@gmail.com>
2026-01-16 01:33:01 -0300

    Urls, views y templates y estrucutra.

--- core/urls.py
+++ core/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("courses/", include("apps.courses.urls")),
     path('dashboard/', include("apps.dashboard.urls")),
+    path('profile/', include("apps.profiles.urls")),
 ]
